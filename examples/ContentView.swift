d6be786658c561c97275d1305d0261a10287e607
Zach Eriksen <zmeriksen@icloud.com>
2022-04-19 18:09:19 -0600

    Initial Commit

---
+++ examples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  examples
+//
+//  Created by Leif on 4/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ examples/examplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  examplesApp.swift
+//  examples
+//
+//  Created by Leif on 4/19/22.
+//
+
+import SwiftUI
+
+@main
+struct examplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
